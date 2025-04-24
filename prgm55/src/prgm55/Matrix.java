0c18132d1fb0b1c849dcf5a3423ab4944baca868
sushantm1 <sushantm1100202@gmail.com>
2025-04-24 22:25:43 +0530

    55 prgms

---
+++ prgm55/src/prgm55/Matrix.java
@@ -0,0 +1,22 @@
+package prgm55;
+
+public class Matrix {
+	public static void mani(String[] args) {
+		int firstmat[][]= {{1,2},
+						   {3,4}};
+		int secmat[][]= {{1,2},
+						{3,4}};
+		int sum[][]=new int[2][2];
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++) {
+				sum[i][j]=firstmat[i][j]+secmat[i][j];
+			}
+		}
+		for(int i=0;i<2;i++){
+			for(int j=0;j<2;j++) {
+				System.out.println(sum[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
--- prgm55/src/prgm55/Matrix_32.java
+++
@@ -1,7 +0,0 @@
-package prgm55;
-// how to ad matrix in java
-public class Matrix_32 {
-	public static void mani(String[] args) {
-		int firstmat[][]=[]
-	}
-}
